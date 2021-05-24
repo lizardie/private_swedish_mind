04339ba96b8cde7205bdba172a73dda928905528
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2021-05-24 16:00:40 +0200

    class based version

--- private_swedish_mind/rings.py
+++ private_swedish_mind/rings.py
@@ -24,7 +24,7 @@ G4_ANTENNAS_PATH = "antennas/LTE.CSV.gz"
 
 SWEREF_EPSG_uppsala =  (647742, 6638924.00)
 
-pd.set_option('display.max_colwidth', None)
+# pd.set_option('display.max_colwidth', None)
 
 SVERIGE_CONTOUR = 'sverige_contour/sverige.shp'
 ALLA_KOMMUNER  = 'alla_kommuner/alla_kommuner.shp'
@@ -39,88 +39,289 @@ SE_SHAPEFILES = 'se_shapefiles/se_1km.shp'
 DATA_DIR = 'data'
 
 
-def prepare_antennas():
+class AnalyseBasicJoinedData:
     """
-    reads and prepares antennas.
-    Returns a GeoPandas df with the antennas.
-    """
-
-    try:
-        g3 = pd.read_csv(G3_ANTENNAS_PATH, sep=';')[['llat', 'llong']]
-        g4 = pd.read_csv(G4_ANTENNAS_PATH, sep=';')[['llat', 'llong']]
-
-        antennas = pd.concat([g3, g4]).drop_duplicates().round(3)
-
-        antennas_gdp = gpd.GeoDataFrame(antennas, geometry=gpd.points_from_xy(antennas.llong, antennas.llat),
-                                        crs=SOURCE_EPSG)[['geometry']] \
-            .to_crs(SWEREF_EPSG)
-
-        return antennas_gdp
-    except:
-        print("something is wrong with the antennas files")
-
-        return None
-
-
-def find_area(point):
-    """
-    given a point  within the area find that area in the table
-    Returns geoPandas DF with the geometry
+    Class handles input  files with the schema: `['timestamp', 'geometry_gps_csv', 'geometry_mpn_csv']`,
+    where `timestamp` is a timestamp, `geometry_gps_csv` is  string representation of a list of length one, which element is a position in a
+    `WGS84_EPSG` projection. The `geometry_mpn_csv` is a string representation of a list  which element is a position in a
+    `WGS84_EPSG` projection.
     """
 
-    try:
-        sweden = gpd.read_file(ALLA_KOMMUNER)
-        sweden.crs = SWEREF_EPSG
-    except:
-        print('failed to read %s' % ALLA_KOMMUNER)
+    def __init__(self, point):
+        """
+        constructor
+        """
+        print("reading data")
+        self.df = self.read_data()
+        self.antennas_data = self.prepare_antennas()
+        print('making bounding area')
+        self.contour = self._get_bounding_area(point=point)
 
-    uppsala = Point(point)
+        print("antennas before filtering ",self.antennas_data.shape)
+        print('filtering antennas data to be inside contour')
+        self.antennas_data = self.get_objects_within_area(self.antennas_data)
+        print("antennas after filtering ", self.antennas_data.shape)
 
-    uppsala_lan = None
-    for index, row in sweden.iterrows():
-        if uppsala.within(sweden.iloc[index].geometry):
-            uppsala_lan = sweden.iloc[index:index + 1].reset_index()
+        print('making voronoi polygons')
+        self.vcs = self.create_voronoi_polygons().to_crs(WGS84_EPSG)
 
-    if not uppsala_lan.empty:
-        return uppsala_lan
-    else:
-        print('failed to find point %s in %s' % (point, ALLA_KOMMUNER))
+    @staticmethod
+    def read_data():
+        """
+        reading data from the folder `DATA_DIR` which start with `result`.
+        Returns Pandas DF, as a concatenation of all files
+
+        :param dt: Data path
+        :return: Pandas DF with `['timestamp', 'geometry_gps_csv', 'geometry_mpn_csv']`  columns
+        """
+
+        paths = sorted(
+            [os.path.join(DATA_DIR, f) for f in os.listdir(DATA_DIR) if f.startswith('result')]
+        )
+
+        dfs = []
+
+        for path in paths:
+            try:
+                tmp = pd.read_csv(path, parse_dates=['timestamp'])[
+                    ['timestamp', 'geometry_gps_csv', 'geometry_mpn_csv']]
+                print(tmp.shape[0], path)
+                #         tmp = tmp.drop_duplicates(subset=['geometry_gps_csv'])
+                #         print(tmp.shape[0], path)
+                dfs.append(tmp)
+            except IOError:
+                print('path %s is wrong. check it.' % path)
+                pass
+            except KeyError:
+                print("some keys are  missing... check it at: %s" % path)
+                pass
+
+        return pd.concat(dfs, axis=0, ignore_index=True)
+
+    def process_df(self, ):
+        """
+        transforms string representation of a list to a Shapely point, and
+        groups the data for each timestamp.
+
+        :return: GeoPandas DF with `timestamp`, Point(), [Point(), Point(), ... Point()] schema
+        """
+        df = self.df
+
+        self.df[geometry_gps_csv] = self.df[geometry_gps_csv].apply(
+            lambda lst: Point(float(lst[1:-1].split(',')[0]),
+                              float(lst[1:-1].split(',')[1]))
+        )
+        self.df[geometry_mpn_csv] = self.df[geometry_mpn_csv].apply(
+            lambda lst: Point(float(lst[1:-1].split(',')[0]),
+                              float(lst[1:-1].split(',')[1]))
+        )
+        self.df = self.df.groupby('timestamp').agg({geometry_gps_csv: "first", geometry_mpn_csv: list})
+
+        self.df = gpd.GeoDataFrame(self.df, geometry=geometry_gps_csv, crs=WGS84_EPSG)  # .to_crs(SWEREF_EPSG)
+
+        # return df
 
 
-def get_objects_within_area(antennas, area, geom='geometry'):
-    """
-    given antennas and bounding geometry find which antennas are within the geometry
-    Returns a GeoPandas DF
-    """
-    if antennas.crs == area.crs:
-        antennas_within = []
-        for i, row in antennas.iterrows():
-            if row[geom].within(area.geometry[0]):
-                antennas_within.append(row)
 
-        if antennas_within:
-            antennas_within = gpd.GeoDataFrame(antennas_within, geometry=geom, crs=antennas.crs).reset_index()
+    def process_position_data(self):
+        """
+
+        :return:
+        """
+        print("processing data")
+        self.process_df()
+        print("processed data shape" ,self.df.shape)
+        print('filtering  data to be inside contour')
+
+        print("data before filtering:", self.df.shape)
+        self.df = self.get_objects_within_area(self.df.to_crs(SWEREF_EPSG), geom='geometry_gps_csv').to_crs(WGS84_EPSG)
+        print("data after filtering:", self.df.shape)
+
+        print('adding add_vcs_indexes')
+        self.add_vcs_indexes()
+        print('adding rings column')
+        self.add_rings_column()
+        print("adding hist column")
+        self.add_hist_column()
+
+
+
+    @staticmethod
+    def prepare_antennas():
+        """
+        reads and prepares antennas for whole Sweden.
+        Returns a GeoPandas df with the antennas.
+        """
+
+        try:
+            g3 = pd.read_csv(G3_ANTENNAS_PATH, sep=';')[['llat', 'llong']]
+            g4 = pd.read_csv(G4_ANTENNAS_PATH, sep=';')[['llat', 'llong']]
+
+            antennas = pd.concat([g3, g4]).drop_duplicates().round(3)
+
+            antennas_gdp = gpd.GeoDataFrame(antennas, geometry=gpd.points_from_xy(antennas.llong, antennas.llat),
+                                            crs=SOURCE_EPSG)[['geometry']] \
+                .to_crs(SWEREF_EPSG)
+
+            return antennas_gdp
+        except:
+            print("something is wrong with the antennas files")
+
+            return None
+
+
+    def _get_bounding_area(self, point=None):
+        """
+        returns bounding area.
+        It could be either the whole Sweden, or it's Lan. In the last case one  need to provide a point within that lan in
+        Sweref99 format.
+
+        """
+        if point:
+            contour = self._find_area(point)
         else:
-            print("no antennas found within area")
+            contour = gpd.read_file(SVERIGE_CONTOUR)
+            contour.crs = WGS84_EPSG
+            contour.to_crs(SWEREF_EPSG, inplace=True)
 
-        return antennas_within
-    else:
-        print('Objects have different CRSs')
-        return None
+        return contour
 
 
-def create_voronoi_polygons(antennas, bounding_geometry):
-    """
-    creates Voronoi polygons with antennas as centers, bounded by `bounding_geometry`
 
-    """
-    coords = points_to_coords(antennas.geometry)
+    def _find_area(self, point):
+        """
+        given a point  within the area find that area in the table
+        Returns geoPandas DF with the geometry
+        """
 
-    poly_shapes, pts = voronoi_regions_from_coords(coords, bounding_geometry.geometry[0])
+        sweden = None
+        try:
+            sweden = gpd.read_file(ALLA_KOMMUNER)
+            sweden.crs = SWEREF_EPSG
+        except:
+            print('failed to read %s' % ALLA_KOMMUNER)
 
-    voronoi_polygons = gpd.GeoDataFrame({'geometry': poly_shapes}, crs=SWEREF_EPSG)
+        uppsala = Point(point)
 
-    return voronoi_polygons
+        uppsala_lan = None
+        for index, row in sweden.iterrows():
+            if uppsala.within(sweden.iloc[index].geometry):
+                uppsala_lan = sweden.iloc[index:index + 1].reset_index()
+
+        if not uppsala_lan.empty:
+            return uppsala_lan
+        else:
+            print('failed to find point %s in %s' % (point, ALLA_KOMMUNER))
+
+
+    # def get_vcs_for_bounding_area(self):
+    #     """
+    #     returns Voronoi cells and their centers
+    #     """
+    #     if self.contour.crs == self.antennas_data.crs:
+    #         self.get_objects_within_area()
+    #         voronoi_polygons = self.create_voronoi_polygons()
+    #
+    #         return antennas_within, voronoi_polygons
+    #     else:
+    #         print("objects have differrent CRSs....")
+    #         return None, None
+
+
+    def get_objects_within_area(self, objects, geom='geometry'):
+        """
+        given objects and bounding geometry find which objects are within the geometry
+        Returns a GeoPandas DF
+        """
+        if objects.crs == self.contour.crs:
+            objects_within = []
+            for i, row in objects.iterrows():
+                if row[geom].within(self.contour.geometry[0]):
+                    objects_within.append(row)
+
+            if objects_within:
+                # if hasattr(self, objects):
+                #     print(type(objects))
+                # else:
+                #     print("dfsffd")
+                return gpd.GeoDataFrame(objects_within, geometry=geom, crs=objects.crs).reset_index()
+            else:
+                print("no objects found within area")
+
+            # return objects_within
+        else:
+            print('Objects have different CRSs: %s and %s ' % (objects.crs, self.contour.crs))
+            # return None
+
+
+    def create_voronoi_polygons(self):
+        """
+        creates Voronoi polygons with antennas as centers, bounded by `bounding_geometry`
+
+        """
+        coords = points_to_coords(self.antennas_data.geometry)
+        print(self.antennas_data.crs, self.contour.crs)
+        print(type(self.contour.geometry[0]))
+        poly_shapes, pts = voronoi_regions_from_coords(coords, self.contour.geometry[0])
+
+        voronoi_polygons = gpd.GeoDataFrame({'geometry': poly_shapes}, crs=SWEREF_EPSG)
+
+        return voronoi_polygons
+
+
+
+    def add_vcs_indexes(self):
+        """
+        Adding indexes of Voronoi cell from `vcs` for GPS and MPN points for given `df`
+        """
+
+        if self.df.crs == self.vcs.crs:
+
+            vc_gps_points = []
+            vc_mpn_points = []
+
+            #     temp = vcs.to_crs(WGS84_EPSG).geometry
+            temp = self.vcs.geometry
+
+            for point in self.df.geometry_gps_csv:
+                for i, vc in enumerate(temp):
+                    if (point.within(vc)): vc_gps_points.append(i)
+
+            for points in self.df.geometry_mpn_csv:
+                t = []
+                for point in points:
+                    for i, vc in enumerate(temp):
+                        if (point.within(vc)): t.append(i)
+                vc_mpn_points.append(t)
+            print(self.df.shape, len(vc_gps_points))
+            self.df['vc_index_gps'] = vc_gps_points
+            self.df['vc_index_mpn'] = vc_mpn_points
+
+            # return df
+        else:
+            print('Objects have different CRSs: %s and %s '%(self.df.crs, self.vcs.crs))
+            # return None
+
+
+
+    def add_rings_column(self):
+        """
+        constructing rings column
+        :return:
+        """
+        tmp = self.vcs.to_crs(WGS84_EPSG)
+
+        self.df['vc_gps_rings'] = self.df.apply(lambda row: get_rings_around_cell(row['vc_index_gps'], tmp, 6), axis=1)
+
+
+    def add_hist_column(self):
+        """
+        adding histogram column
+        :return:
+        """
+
+        self.df['hist'] = self.df.apply(lambda row: make_hist_mpn_geoms(row['vc_index_mpn'], row['vc_gps_rings']),
+                                        axis=1)
 
 
 def get_rings_around_cell(cell_idx, vcs, width):
@@ -160,119 +361,9 @@ def make_hist_mpn_geoms(mpn_geoms, cell_rings):
     return hist
 
 
-def add_vcs_indexes(df, vcs):
-    """
-    Adding indexes of Voronoi cell from `vcs` for GPS and MPN points for given `df`
-    """
-
-    if df.crs == vcs.crs:
-
-        vc_gps_points = []
-        vc_mpn_points = []
-
-        #     temp = vcs.to_crs(WGS84_EPSG).geometry
-        temp = vcs.geometry
-
-        for point in df.geometry_gps_csv:
-            for i, vc in enumerate(temp):
-                if (point.within(vc)): vc_gps_points.append(i)
-
-        for points in df.geometry_mpn_csv:
-            t = []
-            for point in points:
-                for i, vc in enumerate(temp):
-                    if (point.within(vc)): t.append(i)
-            vc_mpn_points.append(t)
-
-        df['vc_index_gps'] = vc_gps_points
-        df['vc_index_mpn'] = vc_mpn_points
-
-        return df
-    else:
-        print('Objects have different CRSs')
-        return None
-
-
-def read_data(dt):
-    """
-    reading data from the folder `DATA_DIR` which start with `result`.
-    Returns Pandas DF, as a concatenation of all files
-
-    :param dt: Data path
-    :return: Pandas DF with `['timestamp', 'geometry_gps_csv', 'geometry_mpn_csv']`  columns
-    """
-
-    paths = sorted(
-        [os.path.join(dt,f) for f in os.listdir(dt) if f.startswith('result')]
-    )
-
-    dfs = []
-
-    for path in paths:
-        try:
-            tmp = pd.read_csv(path, parse_dates=['timestamp'])[['timestamp', 'geometry_gps_csv', 'geometry_mpn_csv']]
-            print(tmp.shape[0], path)
-            #         tmp = tmp.drop_duplicates(subset=['geometry_gps_csv'])
-            #         print(tmp.shape[0], path)
-            dfs.append(tmp)
-        except IOError:
-            print('path %s is wrong. check it.' % path)
-            pass
-        except KeyError:
-            print("some keys are  missing... check it at: %s" %path)
-            pass
-
-    return pd.concat(dfs, axis=0, ignore_index=True)
-
-
-def process_df(df):
-    df[geometry_gps_csv] = df[geometry_gps_csv].apply(
-        lambda lst: Point(float(lst[1:-1].split(',')[0]),
-                          float(lst[1:-1].split(',')[1]))
-    )
-    df[geometry_mpn_csv] = df[geometry_mpn_csv].apply(
-        lambda lst: Point(float(lst[1:-1].split(',')[0]),
-                          float(lst[1:-1].split(',')[1]))
-    )
-    df = df.groupby('timestamp').agg({geometry_gps_csv: "first", geometry_mpn_csv: list})
-
-    df = gpd.GeoDataFrame(df, geometry=geometry_gps_csv, crs=WGS84_EPSG)  # .to_crs(SWEREF_EPSG)
-
-    return df
 
 
 
-def get_vcs_for_bounding_area(antennas, bounding_shape):
-    """
-    returns Voronoi cells and their centers
-    """
-    if  bounding_shape.crs == antennas.crs:
-        antennas_within = get_objects_within_area(antennas, bounding_shape)
-        voronoi_polygons = create_voronoi_polygons(antennas_within, bounding_shape)
-
-
-        return antennas_within, voronoi_polygons
-    else:
-        print("objects have differrent CRSs....")
-        return None,None
-
-
-
-def get_bounding_area(point=None):
-    """
-    returns bounding area.
-    It could be either the whole Sweden, or it's Lan. In the last case one  need to provide a point within that lan in
-    Sweref99 format.
-
-    """
-    if point:
-        contour = find_area(point)
-    else:
-        contour = gpd.read_file(SVERIGE_CONTOUR)
-        contour.crs = WGS84_EPSG
-        contour.to_crs(SWEREF_EPSG, inplace=True)
-
-    return contour
 
 
 def _plot_ring_hist(data):
@@ -308,27 +399,33 @@ if __name__ == "__main__":
         g = t.geometry.unary_union
         gpd.GeoDataFrame(geometry=gpd.GeoSeries(g)).to_file(SVERIGE_CONTOUR)
 
-    antennas_gdp = prepare_antennas()
 
-    # sweden = get_bounding_area()
-    uppsala_lan = get_bounding_area(point=SWEREF_EPSG_uppsala)
+    data = AnalyseBasicJoinedData(point=SWEREF_EPSG_uppsala)
 
-    # antennas_within, voronoi_polygons = get_vcs_for_bounding_area(antennas_gdp, sweden)
-    antennas_within, voronoi_polygons = get_vcs_for_bounding_area(antennas_gdp, uppsala_lan)
+    data.process_position_data()
 
-    read_df = read_data(DATA_DIR)
-    read_df = process_df(read_df.copy())
-    print(type(uppsala_lan), type(read_df))
-    read_df = get_objects_within_area(read_df.copy(), uppsala_lan.to_crs(WGS84_EPSG), geom='geometry_gps_csv')
-
-    read_df = add_vcs_indexes(read_df.copy(), voronoi_polygons.to_crs(WGS84_EPSG))
-
-    tmp = voronoi_polygons.to_crs(WGS84_EPSG)
-
-    read_df['vc_gps_rings'] = read_df.apply(lambda row: get_rings_around_cell(row['vc_index_gps'], tmp, 6), axis=1)
-
-    read_df['hist'] = read_df.apply(lambda row: make_hist_mpn_geoms(row['vc_index_mpn'], row['vc_gps_rings']), axis=1)
-
-    _plot_ring_hist(read_df['hist'])
+    # antennas_gdp = prepare_antennas()
+    #
+    # # sweden = get_bounding_area()
+    # uppsala_lan = get_bounding_area(point=SWEREF_EPSG_uppsala)
+    #
+    #
+    # # antennas_within, voronoi_polygons = get_vcs_for_bounding_area(antennas_gdp, sweden)
+    # antennas_within, voronoi_polygons = get_vcs_for_bounding_area(antennas_gdp, uppsala_lan)
+    #
+    # read_df = read_data()
+    # read_df = process_df(read_df.copy())
+    # print(type(uppsala_lan), type(read_df))
+    # read_df = get_objects_within_area(read_df.copy(), uppsala_lan.to_crs(WGS84_EPSG), geom='geometry_gps_csv')
+    #
+    # read_df = add_vcs_indexes(read_df.copy(), voronoi_polygons.to_crs(WGS84_EPSG))
+    #
+    # tmp = voronoi_polygons.to_crs(WGS84_EPSG)
+    #
+    # read_df['vc_gps_rings'] = read_df.apply(lambda row: get_rings_around_cell(row['vc_index_gps'], tmp, 6), axis=1)
+    #
+    # read_df['hist'] = read_df.apply(lambda row: make_hist_mpn_geoms(row['vc_index_mpn'], row['vc_gps_rings']), axis=1)
+    #
+    # _plot_ring_hist(read_df['hist'])
 
     print('done')
